6c150c8b7c7464fa219ba2cb82d44a615031fae5
Alfa - Andressa Souza <45293704+alfasou@users.noreply.github.com>
2022-04-18 09:37:50 -0300

    Add files via upload
    A simple Blog Rich Text Editor with Title and Subtitle fields, category selector and tags. I used fontawesome icons for the buttons.

---
+++ rich-text-editor.js
@@ -0,0 +1,42 @@
+/* code by webdevtrick ( https://webdevtrick.com ) */
+function enableEditMode() {
+	richTextField.document.designMode = "on";
+  }
+function Edit(command) {
+	richTextField.document.execCommand(command, false, null);
+  }
+function execVal(command, value) {
+	richTextField.document.execCommand(command, false, value);
+  }
+
+
+let input, hashtagArray, container, t;
+
+input = document.querySelector('#hashtags');
+container = document.querySelector('.tag-container');
+hashtagArray = [];
+
+input.addEventListener('keyup', () => {
+    if (event.which == 13 && input.value.length > 0) {
+      var text = document.createTextNode(input.value);
+      var p = document.createElement('p');
+      container.appendChild(p);
+      p.appendChild(text);
+      p.classList.add('tag');
+      input.value = '';
+      
+      let deleteTags = document.querySelectorAll('.tag');
+      
+      for(let i = 0; i < deleteTags.length; i++) {
+        deleteTags[i].addEventListener('click', () => {
+          container.removeChild(deleteTags[i]);
+        });
+      }
+    };
+
+});
+
+if (hashtagArray >= 5) {
+  hashtagArray.slice(0, 5)
+};
+
